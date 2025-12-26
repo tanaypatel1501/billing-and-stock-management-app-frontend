92bcb222800aa27ca60a756209716aa4e4867c9b
tanaypatel1501 <tanaypatel1501@gmail.com>
2025-12-26 17:34:12 +0530

    Landing page improved

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  scrollToFeatures(element: HTMLElement) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 }
